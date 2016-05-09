a0990d9d7154fa98a1928df54456cd6798dc02e5
azhargiri <azhargiri@gmail.com>
2016-05-09 15:48:36 +0700

    Only import STARTUPINFO and STARTF_USESHOWWINDOW on Windows

--- sift.py
+++ sift.py
@@ -2,7 +2,11 @@ from PIL import Image
 import os
 import numpy as np
 import pylab
-from subprocess import Popen, PIPE, STARTUPINFO, STARTF_USESHOWWINDOW 
+from subprocess import Popen, PIPE
+
+# windows specific
+if os.name == 'nt':
+    from subprocess import STARTUPINFO, STARTF_USESHOWWINDOW
 
 def process_image(imagename,resultname,params="--edge-thresh 10 --peak-thresh 5"):
 	""" Process an image and save the results in a file. """
@@ -15,22 +19,22 @@ def process_image(imagename,resultname,params="--edge-thresh 10 --peak-thresh 5"
         # run the sift.exe file
 	cmmd = str("sift "+imagename+" --output="+resultname+" "+params)
 	if os.name == 'nt':     # only on windows
-		startupinfo = STARTUPINFO() 
+		startupinfo = STARTUPINFO()
 		startupinfo.dwFlags |= STARTF_USESHOWWINDOW
 	p = Popen(cmmd, stdout = PIPE, startupinfo=startupinfo) # run sift.exe in subprocess (no separate window opens)
 	p.wait() # wait until subprocess finished
-	
+
 def read_features_from_file(filename):
 	""" Read feature properties and return in matrix form. """
-	
+
 	f = np.loadtxt(filename)
 	if np.ndim(f) < 2:
-		return None, None	 
+		return None, None
 	else:
 		return f[:,:4],f[:,4:] # feature locations, descriptors
 
 def write_features_to_file(filename,locs,desc):
 	""" Save feature location and descriptor to file. """
 	savetxt(filename,hstack((locs,desc)))
-	
+
 
